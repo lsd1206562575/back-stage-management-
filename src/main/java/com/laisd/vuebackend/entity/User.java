8711e12f31a776d82aa4077face2a0047ee4d113
laisd <shudonglai98@gmail.cm>
2022-07-11 10:12:49 -0400

    前端整合echart

--- src/main/java/com/laisd/vuebackend/entity/User.java
+++ src/main/java/com/laisd/vuebackend/entity/User.java
@@ -7,6 +7,8 @@ import com.baomidou.mybatisplus.annotation.TableName;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 
+import java.util.Date;
+
 @Data
 @TableName(value = "sys_user")
 public class User {
@@ -19,4 +21,6 @@ public class User {
     private String email;
     private String phone;
     private String address;
+    @TableField(value = "createtime")
+    private Date createTime;
 }
